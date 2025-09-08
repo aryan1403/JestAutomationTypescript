5a9fff07b8305fd2d7012b0430a853fe73a36742
aryan1403 <aaryan14032006@email.com>
2025-09-08 11:18:07 +0530

    types & functions

---
+++ func.ts
@@ -0,0 +1,8 @@
+// function - functions are peice of code that can be reused
+
+function add(a: number, b: number): string {
+    return "Sum is " + (a + b);
+}
+
+let result = add(5, 10);
+console.log(`${result}`);
